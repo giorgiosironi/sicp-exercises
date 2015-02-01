d96ce75c9e1b03fc7056a6e1bad66fec29a85c58
Giorgio Sironi <info@giorgiosironi.com>
2015-02-01 10:31:32 +0100

    Correct rewrite of 1.16

--- es1.16.scm
+++ es1.16.scm
@@ -1,9 +1,15 @@
 (define (fast-expt b n)
-  (fast-expt-iter b n))
+  (fast-expt-iter b n 1))
 
-(define (fast-expt-iter b n)
-  (if (= n 1)
-      b 
-      (fast-expt-iter (* b b)
-                      (/ n 2))))
+(define (even? n)
+  (= 0 (remainder n 2)))
+
+(define (fast-expt-iter b n a)
+  (cond ((= n 0) a) 
+        ((even? n) (fast-expt-iter (* b b)
+                                   (/ n 2)
+                                   a))
+        (else (fast-expt-iter b
+                              (- n 1)
+                              (* b a)))))
 

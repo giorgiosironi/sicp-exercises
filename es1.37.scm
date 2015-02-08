640e4734ba74bb28b0c78a4b22e17be217fcb390
Giorgio Sironi <info@giorgiosironi.com>
2015-02-08 14:50:11 +0100

    Iterative version of cont-frac

--- es1.37.scm
+++ es1.37.scm
@@ -6,4 +6,19 @@
       (/ (n i)
          (+ (d i)
             (cont-frac-step n d k (+ i 1))))))
-; (/ 1(cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 32)) gives 1.6180339, good
+; (/ 1 (cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 32)) 
+; gives 1.6180339, good
+
+(define (cont-frac-iter n d k)
+  (cont-frac-iter-step n d k 0))
+(define (cont-frac-iter-step n d k k-term)
+  (if (= 0 k)
+      k-term
+      (cont-frac-iter-step n
+                           d
+                           (- k 1)
+                           (/ (n k)
+                              (+ (d k)
+                                 k-term)))))
+; (/ 1 (cont-frac-iter (lambda (i) 1.0) (lambda (i) 1.0) 32))
+; gives 1.6180339, good too

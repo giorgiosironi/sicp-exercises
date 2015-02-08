baefe9ed306997fe5aa72e3afa7e931f82e2732a
Giorgio Sironi <info@giorgiosironi.com>
2015-02-08 13:25:40 +0100

    Iterative version is able to calculate (pi 100000), although it is an unreadable fraction that overflows on conversion to float

--- es1.31.scm
+++ es1.31.scm
@@ -1,8 +1,9 @@
 (define (product term a next b)
-  (if (> a b)
-    1
-    (* (term a)
-       (product term (next a) next b))))
+  (define (iter i result)
+    (if (> i b)
+        result
+        (iter (next i) (* result i))))
+  (iter a 1))
 (define (factorial n)
   (define (identity x) x)
   (define (inc x) (+ 1 x))

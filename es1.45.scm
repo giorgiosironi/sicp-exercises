1739b7d492ecf0cc4c4485ad16aa004665951ce9
Giorgio Sironi <info@giorgiosironi.com>
2015-02-22 14:39:05 +0100

    Researching number of necessary average dampings

--- es1.45.scm
+++ es1.45.scm
@@ -30,3 +30,22 @@
              (- n 1))))
   (fixed-point ((repeated average-damp dampings) transformed) 1.0))
 (define (square x) (* x x))
+; (n-root 2 1 ...) converges
+; (n-root 3 1 ...) converges
+; (n-root 4 1 ...) does not converge
+; (n-root 4 2 ...) converges
+; (n-root 5 2 ...) converges
+; (n-root 6 2 ...) converges
+; (n-root 7 2 ...) converges
+; (n-root 8 2 ...) does not converge
+; (n-root 8 3 ...) converges
+; ...
+; (n-root 15 3 ...) converges
+; (n-root 16 3 ...) does not converge
+; (n-root 16 4 ...) converges
+; ...
+; (n-root 31 4 ...) converges
+; (n-root 32 4 ...) does not converge
+; (n-root 32 5 ...) converges
+; It seems we need a number of dampings which is O(log(n)), in particular here
+; it is >= (floor (log2 n))

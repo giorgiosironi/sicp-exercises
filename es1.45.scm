b9eb6b76127cc3c838c19656f60dc07161323101
Giorgio Sironi <info@giorgiosironi.com>
2015-02-22 14:28:25 +0100

    (n-root n dampings x) allows to experiment with n-root with variable dampings

---
+++ es1.45.scm
@@ -0,0 +1,32 @@
+(define (compose f g)
+  (lambda (x)
+    (f (g x))))
+(define (repeated proc n)
+  (if (= n 0)
+      (lambda (x) x)
+      (compose proc
+               (repeated proc (- n 1)))))
+(define (fixed-point f first-guess)
+  (define tolerance 0.00001)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (newline)
+      (display next)
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+(define (average-damp proc)
+  (lambda (x)
+    (/ (+ x
+          (proc x))
+       2)))
+(define (n-root n dampings x)
+  (define (transformed y)
+    (/ x
+       (expt y 
+             (- n 1))))
+  (fixed-point ((repeated average-damp dampings) transformed) 1.0))
+(define (square x) (* x x))

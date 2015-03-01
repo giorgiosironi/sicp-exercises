364eb9f355aa76126d44ae16c3d75b673dc20289
Giorgio Sironi <info@giorgiosironi.com>
2015-03-01 15:22:38 +0100

    Showing we can sum percentage tolerances

---
+++ es2.13.scm
@@ -0,0 +1,18 @@
+(a +- pa)
+(b +- pb)
+- actually:
+(a - pa*a; a + pa*a)
+(b - pb*b; b + pb*b)
+- product upper bound:
+(a + pa*a) * (b + pb*b)
+ab + pa*a*b + a*pb*b + pa*pb*a*b
+- product center:
+ab
+- difference
+pa*a*b + a*pb*b + pa*pb*a*b
+- given pa*pb is very small:
+pa*a*b + a*pb*b
+- and so
+(pa+pb) * (a*b)
+- which means the percent tolerance sums during product operation
+

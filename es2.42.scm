dc24eeba850a6c44814052032a97613b14379ed5
Giorgio Sironi <info@giorgiosironi.com>
2015-03-15 15:53:49 +0100

    Introduced conflict? to encapsulate queens conflict test

--- es2.42.scm
+++ es2.42.scm
@@ -27,22 +27,40 @@
   (cadr queen))
 ; data structure: positions
 (define empty-board (list))
-(define (rightest-column positions)
-  (column (car positions)))
-(define (all-but-last-columns positions)
-  (map column (cdr positions)))
+(define (rightest-queen positions)
+  (car positions))
+(define (all-but-last positions)
+  (cdr positions))
 (define (adjoin-position new-row k rest-of-queens)
   (cons (make-queen k new-row) rest-of-queens))
+; presentation
 (define (readable queen)
   (display "column: ")
   (display (column queen))
   (display ", row: ")
   (display (row queen))
   (newline))
+(define (dump solutions)
+  (map (lambda (positions)
+         (map readable positions)
+         (display "---")
+         (newline))
+       solutions))
 ; specific operations
+(define (conflict? queen1 queen2)
+  (or (= (column queen1)
+         (column queen2))
+      (= (row queen1)
+         (row queen2))
+      ; same "principal" diagonal
+      (= (- (row queen2) (row queen1))
+         (- (column queen2) (column queen1)))
+      ; trying out same "other" diagonal
+      (= (- (row queen2) (row queen1))
+         (- (column queen1) (column queen2)))))
 (define (safe? k positions)
   (eq? (list)
-       (filter (lambda (queen-in-conflict) (= queen-in-conflict
-                                              (rightest-column positions)))
-               (all-but-last-columns positions))))
+       (filter (lambda (queen-in-conflict) (conflict? queen-in-conflict
+                                                      (rightest-queen positions)))
+               (all-but-last positions))))
 

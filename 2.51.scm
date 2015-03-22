57e75f977583dc98921a44da705a84b02b8c0969
Giorgio Sironi <info@giorgiosironi.com>
2015-03-22 20:30:38 +0100

    Defined two versions of below

---
+++ 2.51.scm
@@ -0,0 +1,21 @@
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((paint-bottom
+            (transform-painter painter1
+                               (make-vect 0.0 0.0)
+                               (make-vect 1.0 0.0)
+                               split-point))
+          (paint-top
+            (transform-painter painter2 
+                               split-point
+                               (make-vect 1.0 0.5)
+                               (make-vect 0.0 1.0))))
+      (lambda (frame)
+        (paint-bottom frame)
+        (paint-top frame)))))
+
+; let's try defining it in term of beside instead...
+(define (below painter1 painter2)
+  (let ((rotated1 (rotate270 painter1))
+        (rotated2 (rotate270 painter2)))
+    (rotate90 (beside rotated1 rotated2))))

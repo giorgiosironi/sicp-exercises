773dd42409720523a56da4854b676034f64a85fb
Giorgio Sironi <info@giorgiosironi.com>
2015-03-22 20:23:44 +0100

    Implemented several painter transformation

---
+++ es2.50.scm
@@ -0,0 +1,17 @@
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(define (rotate-180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+(define (rotate-270-counterclockwise painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))

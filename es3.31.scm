34ef17e00f9ab9caf174d60d6c025e539a415aa6
Giorgio Sironi <info@giorgiosironi.com>
2015-07-05 10:39:30 +0200

    Explanation of accept-action-procedure! implementation

---
+++ es3.31.scm
@@ -0,0 +1,2 @@
+; the proc passed to accept-action-procedure! adds actions to the agenda with a certain delay
+; if we do not run it, no actions will be added and all the signals will remain with their present value

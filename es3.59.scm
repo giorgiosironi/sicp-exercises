c204041502235809dd023a342d0ec829d69bc19d
Giorgio Sironi <info@giorgiosironi.com>
2015-08-09 11:07:23 +0200

    Computing cosine-series and sine-series

--- es3.59.scm
+++ es3.59.scm
@@ -2,6 +2,11 @@
 (define (integers-starting-from n)
   (cons-stream n (integers-starting-from (+ n 1))))
 (define integers (integers-starting-from 1))
+(define (scale-stream s n)
+  (cons-stream (* (stream-car s)
+                  n)
+               (scale-stream (stream-cdr s)
+                             n)))
 (define (divide-streams s1 s2)
   (cons-stream (/ (stream-car s1)
                   (stream-car s2))
@@ -12,3 +17,9 @@
   (divide-streams
     series
     integers))
+(define exp-series
+  (cons-stream 1 (integrate-series exp-series)))
+(define cosine-series
+  (cons-stream 1 (integrate-series (scale-stream sine-series -1))))
+(define sine-series
+  (cons-stream 0 (integrate-series cosine-series)))

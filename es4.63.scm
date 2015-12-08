3222b20757407552334db2bf5ec3d2e6cf7bb9d0
Giorgio Sironi <info@giorgiosironi.com>
2015-12-08 13:36:02 +0100

    grandson and additional son rules

---
+++ es4.63.scm
@@ -0,0 +1,9 @@
+(rule (grandson ?g s?)
+      (and (son ?f ?s)
+           (son ?g ?f)))
+(rule (son m? s?)
+      (and (son f? s?)
+           (wife m? w?)))
+; grandson of Cain: Irad
+; sons of Lamech: Jabal, Jubal
+; grandsons of Methushael: Jabal, Jubal

12a2e3d81722ef61a95eb5d32742ac870f06024e
Giorgio Sironi <info@giorgiosironi.com>
2015-12-08 09:45:33 +0100

    Rule for meeting times

---
+++ es4.59.scm
@@ -0,0 +1,6 @@
+(meeting ?m (Friday ?time))
+(rule (meeting-time ?person ?day-and-time)
+      (and (job ?person (?department . ?))
+           (or (meeting ?department ?day-and-time)
+               (meeting whole-company ?day-and-time))))
+(meeting-time (Hacker Alyssa P.) (Wednesday ?time))

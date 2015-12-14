24343c47922f31f08654130ad2d870a385db72bb
Giorgio Sironi <info@giorgiosironi.com>
2015-12-14 09:39:02 +0100

    First part of the machine simulator

---
+++ es5.8.scm
@@ -0,0 +1,23 @@
+(load "chapter5.2.scm")
+(define ambiguous-code
+  '(start
+    (goto (label here)e
+    here
+    (assign a (const 3))
+    (goto (label there))
+    here
+    (assign a (const 4))
+    (goto (label there))
+    there))
+(define (receive-dump insts labels)
+  (display "instructions: ")
+  (display insts)
+  (newline)
+  (display "labels: ")
+  (display labels)
+  (newline))
+(extract-labels ambiguous-code receive-dump)
+; labels: ((start ((goto (label here))) ((assign a (const 3))) ((goto (label there))) ((assign a (const 4))) ((goto (label there)))) (here ((assign a (const 3))) ((goto (label there))) ((assign a (const 4))) ((goto (label there)))) (here ((assign a (const 4))) ((goto (label there)))) (there))
+; the first (here ...) shadows the second in the association list,
+; so it will always assign 3
+

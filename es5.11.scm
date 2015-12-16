0fa2a174c8b8a9030ffa6a945a3c2232d8faff3d
Giorgio Sironi <info@giorgiosironi.com>
2015-12-16 09:19:32 +0100

    Version b of restore

---
+++ es5.11.scm
@@ -0,0 +1,18 @@
+(load "chapter5.2.scm")
+(define (make-save-b inst machine stack pc)
+  (let ((reg-name (stack-inst-reg-name inst)))
+    (let ((reg (get-register machine
+                             reg-name)))
+      (lambda ()
+        (push stack (list reg-name (get-contents reg)))
+        (advance-pc pc)))))
+(define (make-restore-b inst machine stack pc)
+  (let ((reg-name (stack-inst-reg-name inst)))
+    (let ((reg (get-register machine
+                             reg)))
+      (lambda ()
+        (let ((popped (pop stack)))
+          (if (eq? reg-name (car popped))
+              (set-contents! reg (cadr popped))
+              (error "pushed one register but popped on another -- MAKE-RESTORE" (list reg-name popped))))
+        (advance-pc pc)))))

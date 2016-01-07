c14c69df17839c42c2d405ddce43005da89faa0b
Giorgio Sironi <info@giorgiosironi.com>
2016-01-07 09:02:31 +0100

    Compiled and executed append!

--- es5.22.scm
+++ es5.22.scm
@@ -6,6 +6,26 @@
   (if (null? (cdr x))
     x
     (last-pair (cdr x))))
+(define append!-machine-controller
+  '(
+    append!
+    (assign val (reg x))
+    (assign continue (label found-last-pair))
+    (save continue)
+    (goto (label last-pair))
+    found-last-pair
+    (perform (op set-cdr!) (reg x) (reg y))
+    (goto (label end-append!))
+    last-pair
+    (assign temp (op cdr) (reg x))
+    (test (op null?) (reg temp))
+    (branch (label it-s-the-last))
+    (assign x (op cdr) (reg x))
+    (goto (label last-pair))
+    it-s-the-last
+    (restore continue)
+    (goto (reg continue))
+    end-append!))
 ; append to be compiled
 (define (append x y)
   (if (null? x)
@@ -33,6 +53,7 @@
     (assign val (reg y))
     (goto (reg continue))
     end-of-append))
+; testing
 (load "chapter5.2.scm")
 (define append-machine (make-machine '(x y temp val continue)
                                      (list (list 'cons cons)
@@ -49,4 +70,20 @@
 (start append-machine)
 (display "Append result: ")
 (display (get-register-contents append-machine 'val))
+(newline)
+(load "chapter5.2.scm")
+(define append!-machine (make-machine '(x y temp val continue)
+                                     (list (list 'set-cdr! set-cdr!)
+                                           (list 'cdr cdr)
+                                           (list 'null? null?)
+                                           (list 'dump
+                                                 (lambda (x)
+                                                   (display x)
+                                                   (newline))))
+                                     append!-machine-controller))
+(set-register-contents! append!-machine 'x '(1 2 3))
+(set-register-contents! append!-machine 'y '(4 5 6))
+(start append!-machine)
+(display "Append! result: ")
+(display (get-register-contents append!-machine 'val))
 (newline)

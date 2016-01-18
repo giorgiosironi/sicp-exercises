d54b525672955a7efde12849ea9e15a10e23684c
Giorgio Sironi <info@giorgiosironi.com>
2016-01-18 08:46:18 +0100

    Fixing patch to not handle everything as cond expressions

--- es5.23.scm
+++ es5.23.scm
@@ -6,8 +6,12 @@
 (add-operation 'cond->if cond->if)
 (apply-patch '(cond-if-patch-start
                (test (op cond?) (reg exp))
+               (branch (label ev-cond-if))
+               (goto (label cond-if-patch-end))
+               ev-cond-if
                (assign exp (op cond->if) (reg exp))
-               (branch (label ev-if)))
+               (branch (label ev-if))
+               cond-if-patch-end)
              'extensions)
 
 (start eceval)

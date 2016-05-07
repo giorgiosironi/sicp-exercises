b9804e695795f9c332da2954aba19536de596728
Giorgio Sironi <info@giorgiosironi.com>
2016-05-07 13:01:41 +0100

    Putting together the compiler piece by piece: got to 5.5.2

--- chapter5.5.scm
+++ chapter5.5.scm
@@ -30,3 +30,146 @@
   (list needs modifies statements))
 (define (empty-instruction-sequence)
   (make-instruction-sequence '() '() '()))
+; each code generator will end with a linkage:
+; - return (from a procedure application)
+; - next (just go on)
+; - goto (a label)
+(define (compile-linkage linkage)
+  (cond ((eq? linkage 'return)
+         (make-instruction-sequence '(continue) '()
+                                    '((goto (reg continue)))))
+        ((eq? linkage 'next)
+         (empty-instruction-sequence))
+        (else
+          (make-instruction-sequence '() '()
+                                     `((goto (label ,linkage)))))))
+; the linkage code will be joined in to instructions
+; by preserving continue, which is needed in case of 'return
+(define (end-with-linkage linkage instruction-sequence)
+  (preserving '(continue)
+              instruction-sequence
+              (compile-linkage linkage)))
+; compiling simple expressions:
+; - they all modify target
+; - looking upvariables requires env
+(define (compile-self-evaluating exp target linkage)
+  (end-with-linkage linkage
+                    (make-instruction-sequence '() (list target)
+                                               `((assign ,target (const ,exp))))))
+(define (compile-quoted exp target linkage)
+  (end-with-linkage linkage
+                    (make-instruction-sequence '() (list target)
+                                               `((assign ,target (const ,(text-of-quotation exp)))))))
+(define (compile-variable exp target linkage)
+  (end-with-linkage linkage
+                    (make-instruction-sequence '(env) (list target)
+                                               `((assign ,target
+                                                         (op lookup-variable-value)
+                                                         (const ,exp)
+                                                         (reg env))))))
+; assignments and definitions are recursive:
+; - generate code that computes the value
+; - assign it to the target
+(define (compile-assignment exp target linkage)
+  (let ((var (assignment-variable exp))
+        (get-value-code
+          (compile (assignment-value exp) 'val 'next)))
+    (end-with-linkage linkage
+                      (preserving '(env)
+                                  get-value-code
+                                  (make-instruction-sequence '(env val) (list target)
+                                                             `((perform (op set-variable-value!)
+                                                                        (const ,var)
+                                                                        (reg val)
+                                                                        (reg env))
+                                                               (assign ,target (const ok))))))))
+(define (compile-definition exp target linkage)
+  (let ((var (definition-variable exp))
+        (get-value-code
+          (compile (definition-value exp) 'val 'next)))
+    (end-with-linkage linkage
+                      (preserving '(env)
+                                  get-value-code
+                                  (make-instruction-sequence '(env val) (list target)
+                                                             `((perform (op define-variable!)
+                                                                        (const ,var)
+                                                                        (reg val)
+                                                                        (reg env))
+                                                               (assign ,target (const ok))))))))
+; conditionals. Notice if predicate then consequent else alternative
+; notice how consequent is linked to after-if to skip
+; over the alternative
+; env and continue are preserved when evaluating the 
+; predicate, as they can be used by branches
+(define (compile-if exp target linkage)
+  (let ((t-branch (make-label 'true-branch))
+        (f-branch (make-label 'false-branch))
+        (after-if (make-label 'after-if)))
+    (let ((consequent-linkage
+            (if (eq? linkage 'next) after-if linkage)))
+      (let ((p-code (compile (if-predicate exp)
+                             'val 
+                             'next))
+            (c-code
+              (compile
+                (if-consequent exp)
+                target
+                consequent-linkage))
+            (a-code
+              (compile 
+                (if-alternative exp)
+                target
+                linkage)))
+        (preserving '(env continue)
+                    p-code
+                    (append-instruction-sequences
+                      (make-instruction-sequence '(val) '()
+                                                 `((test (op false?) (reg val))
+                                                   (branch (label ,f-branch))))
+                      (parallel-instruction-sequences
+                        (append-instruction-sequences t-branch c-code)
+                        (append-instruction-sequences f-branch a-code))
+                      after-if))))))
+; sequences of statements
+; they are joined to each other with 'next linkages
+; but they need to preserve env (for the following ones)
+; and continue (possibly for the last linkage)
+(define (compile-sequence seq target linkage)
+  (if (last-exp? seq)
+    (compile (first-exp seq) target linkage)(preserving '(env continue)
+                                                        (compile (first-exp seq) target 'next)
+                                                        (compile-sequence (rest-exps seq) target linkage))))
+; lambda expressions: code is inserted here but not immediately executed
+; if the linkage is next, we'll skip over it
+; notice that the lambda is tacked on, which means
+; it's not part of the normal sequence of statements
+(define (compile-lambda exp target linkage)
+  (let ((proc-entry (make-label 'entry))
+        (after-lambda (make-label 'after-lambda)))
+    (let ((lambda-linkage
+            (if (eq? linkage 'next) after-lambda linkage)))
+      (append-instruction-sequences
+        (tack-on-instruction-sequence
+          (end-with-linkage lambda-linkage
+                            (make-instruction-sequence '(env) (list target)
+                                                       `((assign ,target
+                                                                 (op make-compiled-procedure)
+                                                                 (label ,proc-entry)
+                                                                 (reg env)))))
+          (compile-lambda-body exp proc-entry))
+        after-lambda))))
+; compiling the lambda body
+; the new environment is augmented with the formal parameters
+; but the body in itself is just compiled as a sequence
+(define (compile-lambda-body exp proc-entry)
+  (let ((formals (lambda-parameters exp)))
+    (append-instruction-sequences
+      (make-instruction-sequence '(env proc argl) '(env)
+                                 `(,proc-entry
+                                    (assign env (op compiled-procedure-env) (reg proc))
+                                    (assign env
+                                            (op extend-environment)
+                                            (const ,formals)
+                                            (reg argl)
+                                            (reg env))))
+      (compile-sequence (lambda-body exp) 'val 'return))))

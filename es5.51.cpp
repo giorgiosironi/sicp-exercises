b7989d6f72fdb981982fc3babf3d457f43684aa3
Giorgio Sironi <g.sironi@elifesciences.org>
2016-07-09 11:33:36 +0100

    Cons is a Value, and also Nil is one

--- es5.51.cpp
+++ es5.51.cpp
@@ -30,7 +30,7 @@ std::string SchemeInteger::toString()
     return std::to_string(this->contents);
 }
 
-class Cons
+class Cons : public Value
 {
     private:
         Value *car_ptr;
@@ -40,6 +40,7 @@ class Cons
         Cons(Value *car_ptr, Value *cdr_ptr);
         Value* car();
         Value* cdr();
+        virtual std::string toString();
 };
 
 Cons::Cons(Value *car_ptr, Value *cdr_ptr)
@@ -58,9 +59,34 @@ Value* Cons::cdr()
     return this->cdr_ptr;
 }
 
+std::string Cons::toString()
+{
+    return std::string("(")
+        + this->car_ptr->toString()
+        + std::string(" . ")
+        + this->cdr_ptr->toString() 
+        + std::string(")");
+}
+
+class Nil : public Value 
+{
+    public:
+        virtual std::string toString();
+};
+
+std::string Nil::toString()
+{
+    return std::string("NIL");
+}
+
+
+#define NIL (new Nil())
+
 int main() {
     Cons* cell = new Cons(new SchemeInteger(42), new SchemeInteger(43));
     Value* i = cell->car();
     cout << i->toString() << endl;
+    cout << cell->toString() << endl;
+    cout << NIL->toString() << endl;
     return 0;
 }

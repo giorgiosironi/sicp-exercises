aae3341a5f52a8594e9b2377be3357f466b7ec74
Giorgio Sironi <g.sironi@elifesciences.org>
2017-04-08 11:57:20 +0100

    Second end2end test: assignment does not explode

--- es5.51/tests/test_end2end.py
+++ es5.51/tests/test_end2end.py
@@ -2,13 +2,24 @@ from subprocess import Popen, PIPE, STDOUT
 import unittest
 
 class End2endTest(unittest.TestCase):
-    def test_sum(self):
-        p = Popen(['./main'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
-        # not very clear how long this waits
-        (stdout_data, stderr_data) = p.communicate(input='(+ 42 43)')
+    def setUp(self):
+        self._p = Popen(['./main'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
 
-        #print stderr_data
-        output_lines = stdout_data.split("\n")
-        #print output_lines
+    def test_sum(self):
+        self._input('(+ 42 43)')
+
+        output_lines = self._stdout_data.split("\n")
 
         self.assertEqual([l for l in output_lines if l == "85"], ["85"], "There is no line in the output with the result")
+
+    def test_assignment(self):
+        self._input('(define answer 42)')
+
+        output_lines = self._stdout_data.split("\n")
+        print output_lines
+
+        self.assertEqual([l for l in output_lines if l == "'ok"], ["'ok"], "There is no line in the output with the result")
+
+    def _input(self, line):
+        # not very clear how long this waits
+        (self._stdout_data, self._stderr_data) = self._p.communicate(input=line)

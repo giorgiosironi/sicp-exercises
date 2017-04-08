4295c5dd062cb969a9d28bbddb3e1bc580dd3cf8
Giorgio Sironi <g.sironi@elifesciences.org>
2017-04-08 12:02:51 +0100

    Correctly extracting output from interpreter

--- es5.51/tests/test_end2end.py
+++ es5.51/tests/test_end2end.py
@@ -7,19 +7,24 @@ class End2endTest(unittest.TestCase):
 
     def test_sum(self):
         self._input('(+ 42 43)')
-
-        output_lines = self._stdout_data.split("\n")
-
-        self.assertEqual([l for l in output_lines if l == "85"], ["85"], "There is no line in the output with the result")
+        self.assertEqual(self._output(), '85')
 
     def test_assignment(self):
         self._input('(define answer 42)')
-
-        output_lines = self._stdout_data.split("\n")
-        print output_lines
-
-        self.assertEqual([l for l in output_lines if l == "'ok"], ["'ok"], "There is no line in the output with the result")
+        self.assertEqual(self._output(), "'ok")
 
     def _input(self, line):
         # not very clear how long this waits
         (self._stdout_data, self._stderr_data) = self._p.communicate(input=line)
+
+    def _output(self):
+        output_lines = self._stdout_data.split("\n")
+        output_index = None
+        for index, line in enumerate(output_lines):
+            if '>>> ";;; EC-Eval value:"' == line:
+                output_index = index + 1
+                break
+        self.assertIsNotNone(output_index)
+        return output_lines[output_index]
+
+

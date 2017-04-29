30083dd100548f60f49b279c521780c228a94553
Giorgio Sironi <g.sironi@elifesciences.org>
2017-04-29 12:14:53 +0100

    (*) evaluates to 1, it's a reduce too

--- es5.51/src/PrimitiveTimes.cpp
+++ es5.51/src/PrimitiveTimes.cpp
@@ -1,14 +1,18 @@
 #include "primitive_times.h"
 #include "cons.h"
 #include "integer.h"
+#include "is.h"
 #include <iostream>
 using namespace std;
 
 Value* PrimitiveTimes::apply(List* arguments)
 {
     Cons* argumentsCons = (Cons*) arguments;
-    int result = ((Integer*) argumentsCons->car())->as_int()
-             * ((Integer*) argumentsCons->cadr())->as_int();
+    int result = 1;
+    while (is_pair(argumentsCons)) {
+        result *= ((Integer*) argumentsCons->car())->as_int();
+        argumentsCons = (Cons*) argumentsCons->cdr();
+    }
     return new Integer(result);
 }
 

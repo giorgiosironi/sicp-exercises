be8f6b6aa607a4d43e9b5a9304978e5b6d9b20d5
Giorgio Sironi <g.sironi@elifesciences.org>
2017-07-29 11:28:54 +0100

    Renaming internal operation to convert-poly

--- es2.91.scm
+++ es2.91.scm
@@ -89,9 +89,10 @@
       (error "Polys not in same var -- ADD-POLY"
              (list p1 p2))))
   (define (mul-poly p1 p2)
-    (if (same-variable? (variable p1) (variable p2))(make-poly (variable p1)
-                                                               (mul-terms (term-list p1)
-                                                                          (term-list p2)))
+    (if (same-variable? (variable p1) (variable p2))
+      (make-poly (variable p1)
+                 (mul-terms (term-list p1)
+                            (term-list p2)))
       (error "Polys not in same var -- MUL-POLY"
              (list p1 p2))))
   (define (tag p) (attach-tag 'polynomial p))

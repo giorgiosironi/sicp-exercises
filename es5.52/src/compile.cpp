50be6d5d13ad3dc7b9895a894dc9c8eb5574adb9
Giorgio Sironi <g.sironi@elifesciences.org>
2017-11-11 11:59:42 +0000

    Correct checks to switch between self-evaluating and variable

--- es5.52/src/compile.cpp
+++ es5.52/src/compile.cpp
@@ -2,18 +2,23 @@
 // temporary
 #include "nil.h"
 #include "cons.h"
+#include "is.h"
+#include "symbol.h"
 
 InstructionSequence* compile(Value* exp, Symbol* target)
 {
     if (is_self_evaluating(exp)) {
         return compile_self_evaluating(exp, target);
     }
+    if (is_variable(exp)) {
+        return new InstructionSequence(vector<Symbol*>(), vector<Symbol*>(), NIL);
+    }
     return new InstructionSequence(vector<Symbol*>(), vector<Symbol*>(), NIL);
 }
 
 bool is_self_evaluating(Value* exp)
 {
-    return true;
+    return is_number(exp) || is_string(exp);
 }
 
 InstructionSequence* compile_self_evaluating(Value* exp, Symbol* target)
@@ -32,4 +37,12 @@ InstructionSequence* compile_self_evaluating(Value* exp, Symbol* target)
             })
         })
     );
+}
+
+bool is_variable(Value* exp)
+{
+    if (Symbol *symbol = dynamic_cast<Symbol *>(exp)) {
+        return true;
+    }
+    return false;
 }
--- es5.52/src/compile.h
+++ es5.52/src/compile.h
@@ -7,5 +7,6 @@
 InstructionSequence* compile(Value* exp, Symbol* target);
 bool is_self_evaluating(Value* exp);
 InstructionSequence* compile_self_evaluating(Value* exp, Symbol* target);
+bool is_variable(Value *exp);
 
 #endif

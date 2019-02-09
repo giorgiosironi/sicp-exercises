748572ce9a0ce0498c22032a563cfc28a8804507
Giorgio Sironi <g.sironi@elifesciences.org>
2019-02-09 11:48:47 +0000

    Simplify empty new lines

--- es5.52/metacircular-evaluator.lisp
+++ es5.52/metacircular-evaluator.lisp
@@ -86,53 +86,34 @@
       (make-lambda (cdadr exp)   
                    (cddr exp)))) 
 (define (lambda? exp) (tagged-list? exp 'lambda))
-
 (define (lambda-parameters exp) (cadr exp))
-
 (define (lambda-body exp) (cddr exp))
-
 (define (make-lambda parameters body)
   (cons 'lambda (cons parameters body)))
 (define (if? exp) (tagged-list? exp 'if))
-
 (define (if-predicate exp) (cadr exp))
-
 (define (if-consequent exp) (caddr exp))
-
 (define (if-alternative exp)
   (if (not (null? (cdddr exp)))
       (cadddr exp)
       'false))
-
 (define (make-if predicate consequent alternative)
   (list 'if predicate consequent alternative))
 (define (begin? exp) (tagged-list? exp 'begin))
-
 (define (begin-actions exp) (cdr exp))
-
 (define (last-exp? seq) (null? (cdr seq)))
-
 (define (first-exp seq) (car seq))
-
 (define (rest-exps seq) (cdr seq))
-
 (define (sequence->exp seq)
   (cond ((null? seq) seq)
         ((last-exp? seq) (first-exp seq))
         (else (make-begin seq))))
-
 (define (make-begin seq) (cons 'begin seq))
-
 (define (application? exp) (pair? exp))
-
 (define (operator exp) (car exp))
-
 (define (operands exp) (cdr exp))
-
 (define (no-operands? ops) (null? ops))
-
 (define (first-operand ops) (car ops))
-
 (define (rest-operands ops) (cdr ops))
 (define (cond? exp) (tagged-list? exp 'cond))
 (define (cond-clauses exp) (cdr exp))
